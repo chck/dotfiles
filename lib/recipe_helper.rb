c94acf7942c9284bed218bc072ed8db204d843f4
chck <deadline.is.today@gmail.com>
2018-08-26 04:06:26 +0900

    Fix context to perform recipe helper

--- lib/recipe_helper.rb
+++ lib/recipe_helper.rb
@@ -1,4 +1,4 @@
-MItamae::RecipeContext.class_eval do
+Itamae::Recipe::EvalContext.class_eval do
   def include_cookbook(name)
     root_dir = File.expand_path('../..', __FILE__)
     include_recipe File.join(root_dir, 'cookbooks', name, 'default')
