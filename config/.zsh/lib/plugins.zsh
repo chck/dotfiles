20e3937f589c6e12f370f15b27dbac1af264d878
chck <deadline.is.today@gmail.com>
2018-08-26 20:04:13 +0900

    Add zplug plugins: fzf, jq and for zsh users

--- config/.zsh/lib/plugins.zsh
+++ config/.zsh/lib/plugins.zsh
@@ -1,6 +1,13 @@
 source ~/.zplug/init.zsh
 
+zplug "junegunn/fzf-bin", as:command, from:gh-r, rename-to:fzf
 zplug "b4b4r07/enhancd", use:init.sh
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "zsh-users/zsh-autosuggestions", defer:2
+zplug "zsh-users/zsh-completions"
+zplug "zsh-users/zsh-history-substring-search", hook-build:"__zsh_version 5.3"
+zplug "stedolan/jq", from:gh-r, as:command
+zplug "b4b4r07/emoji-cli", if:"which jq"
 
 zplug check --verbose || zplug install
 zplug load
