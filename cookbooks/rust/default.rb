fe1efab20665a445fa04ed44aa13a2ac02ddac44
chck <deadline.is.today@gmail.com>
2018-08-30 14:09:08 +0900

    Add cookbook: rust

---
+++ cookbooks/rust/default.rb
@@ -0,0 +1,32 @@
+case node[:platform]
+when 'arch'
+  package 'rust'
+  package 'cargo'
+
+  include_cookbook 'yaourt'
+  yaourt 'rust-src'
+else
+  local_ruby_block 'install rust' do
+    rustc_path = "#{ENV['HOME']}/.cargo/bin/rustc"
+
+    block do
+      system("sudo -E -u #{node[:user]} bash -c 'curl https://sh.rustup.rs -sSf | sh'")
+
+      until File.exist?(rustc_path)
+        sleep 1
+      end
+    end
+    not_if "test -f #{rustc_path}"
+  end
+end
+
+unless ENV['PATH'].include?("#{ENV['HOME']}/.cargo/bin:")
+  Itamae.logger.info('Prepending ~/.cargo/bin to PATH during this execution')
+  ENV['PATH'] = "#{ENV['HOME']}/.cargo/bin:#{ENV['PATH']}"
+end
+
+define :cargo do
+  execute "cargo install --verbose #{params[:name]}" do
+    not_if %Q[cargo install --list | grep "^#{params[:name]} "]
+  end
+end
