324f516e03aefcd83b408060da38bf3362adb21f
chck <deadline.is.today@gmail.com>
2018-09-01 19:22:26 +0900

    Add cookbook: mecab

---
+++ cookbooks/mecab/default.rb
@@ -0,0 +1,15 @@
+package 'mecab'
+package 'xz' # mecab dependencies
+
+git "install mecab-ipadic-neologd" do
+  repository "https://github.com/neologd/mecab-ipadic-neologd.git"
+  depth 1
+  destination "/tmp/neologd"
+end
+
+directory "/usr/local/lib/mecab/dic" do
+  owner node[:user]
+end
+
+execute "/tmp/neologd/bin/install-mecab-ipadic-neologd -n -u -y && rm -rf /tmp/neologd"
+dotfile 'mecabrc' '/usr/local/etc'
