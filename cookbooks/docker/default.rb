f542b5d4fadd111d3fdcf4f99752c6e02348288d
chck <deadline.is.today@gmail.com>
2018-09-08 21:01:34 +0900

    Add cookbook: docker-clean

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -4,3 +4,7 @@ when 'darwin'
 else
   raise NotImplementedError
 end
+
+github_binary 'docker-clean' do
+  raw_url 'https://raw.githubusercontent.com/ZZROTDesign/docker-clean/v2.0.4/docker-clean'
+end
--- cookbooks/functions/default.rb
+++ cookbooks/functions/default.rb
@@ -11,28 +11,34 @@ define :dotfile, source: nil, destination: nil do
   end
 end
 
-define :github_binary, version: nil, repository: nil, archive: nil, binary_path: nil do
+define :github_binary, raw_url: nil, version: nil, repository: nil, archive: nil, binary_path: nil do
   cmd = params[:name]
-  bin_path = "#{ENV['HOME']}/bin/#{cmd}"
+  bin_path = "/usr/local/bin/#{cmd}"
   archive = params[:archive]
-  url = "https://github.com/#{params[:repository]}/releases/download/#{params[:version]}/#{archive}"
+  url = params[:raw_url] || "https://github.com/#{params[:repository]}/releases/download/#{params[:version]}/#{archive}"
 
-  if archive.end_with?('.zip')
-    extract = "unzip -o"
-  elsif archive.end_with?('.tar.gz')
-    extract = "tar xvzf"
+  if archive
+    if archive.end_with?('.zip')
+      extract = "unzip -o"
+    elsif archive.end_with?('.tar.gz')
+      extract = "tar xvzf"
+    else
+      raise "unexpected ext archive: #{archive}"
+    end
+
+    execute "curl -fSL -o /tmp/#{archive} #{url}" do
+      not_if "test -f #{bin_path}"
+    end
+    execute "#{extract} /tmp/#{archive}" do
+      not_if "test -f #{bin_path}"
+      cwd "/tmp"
+    end
   else
-    raise "unexpected ext archive: #{archive}"
+    execute "curl -fSL -o /tmp/#{cmd} #{url}" do
+      not_if "test -f #{bin_path}"
+    end
   end
-
-  execute "curl -fSL -o /tmp/#{archive} #{url}" do
-    not_if "test -f #{bin_path}"
-  end
-  execute "#{extract} /tmp/#{archive}" do
-    not_if "test -f #{bin_path}"
-    cwd "/tmp"
-  end
-  execute "mv /tmp/#{params[:binary_path] || cmd} #{bin_path} && chmod +x #{path}" do
+  execute "mv /tmp/#{params[:binary_path] || cmd} #{bin_path} && chmod +x #{bin_path}" do
     not_if "test -f #{bin_path}"
   end
 end
