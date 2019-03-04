a383ea186014a22de22d36c549f20fd8071825fe
chck <deadline.is.today@gmail.com>
2019-03-05 04:31:33 +0900

    Add the command to install dein.vim

--- cookbooks/vim/default.rb
+++ cookbooks/vim/default.rb
@@ -2,14 +2,16 @@ package 'vim' do
   options '--with-lua --with-override-system-vi --with-python3'
 end
 
-#execute 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh)" ~/.vim/bundles'
-
 dotfile '.vim'
 dotfile '.vimrc'
 if node[:platform] == 'darwin'
   dotfile '.ideavimrc' do
     not_if 'test -f ~/.ideavimrc'
   end
+end
+
+execute 'curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > /tmp/installer.sh && sh /tmp/installer.sh ~/.vim/bundles' do
+  not_if 'test -d ~/.vim/bundles/repos/github.com/Shougo/dein.vim'
 end
 
 execute "vim -c 'call dein#install() | q!'" do
