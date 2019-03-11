6a592a63d26b161d2d4c21ade513e61ab60587ae
chck <deadline.is.today@gmail.com>
2019-03-11 19:55:00 +0900

    Add the case insensitive to zsh completion

--- config/.zsh/lib/completion.zsh
+++ config/.zsh/lib/completion.zsh
@@ -6,5 +6,8 @@ compinit
 setopt auto_param_slash
 setopt mark_dirs
 
+# case insensitive
+zstyle ':completion:*' matcher-list '' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}'
+
 # Pipenv
 export PIPENV_VENV_IN_PROJECT=1
