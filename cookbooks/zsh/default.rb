80ac5bcd51d4192fd4820b9da87a8db2b610da67
chck <shimekiri.today@gmail.com>
2019-12-30 03:00:58 +0900

    Refactor condition to apply zsh

--- cookbooks/zsh/default.rb
+++ cookbooks/zsh/default.rb
@@ -1,13 +1,13 @@
 if node[:platform] == 'darwin'
   dotfile '.zshrc.darwin'
   execute "sudo chsh -s /bin/zsh #{node[:user]}" do
-    not_if 'test /bin/zsh = $SHELL'
+    not_if 'test $SHELL == /bin/zsh'
   end
 else
   package 'zsh'
   dotfile '.zshrc.Linux'
   execute "chsh -s /bin/zsh #{node[:user]}" do
-    not_if 'test /bin/zsh = $SHELL'
+    not_if 'test $SHELL == /bin/zsh'
     only_if "getent passwd #{node[:user]} | cut -d: -f7 | grep -q '^/bin/bash$'"
     user 'root'
   end
