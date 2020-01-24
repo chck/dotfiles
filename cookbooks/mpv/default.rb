4faa40ddb6a40685091e18a58dbe810d7f850404
chck <shimekiri.today@gmail.com>
2020-01-24 16:27:53 +0900

    Add default configuration of mpv to play media infinity loop

--- cookbooks/mpv/default.rb
+++ cookbooks/mpv/default.rb
@@ -6,3 +6,11 @@ when 'darwin'
 else
   raise NotImplementedError
 end
+
+execute '''cat <<EOF >> ~/.config/mpv/mpv.conf
+# https://www.reddit.com/r/mpv/comments/9gopck/how_would_one_make_all_files_loop_by_default/
+loop=yes
+EOF
+''' do
+  not_if 'grep loop=yes ~/.config/mpv/mpv.conf'
+end
