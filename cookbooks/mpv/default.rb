a03342c55de96a0413ac99cd1fad05e6bceb390c
chck <shimekiri.today@gmail.com>
2020-04-15 00:53:01 +0900

    Modify the configuration for mpv

--- cookbooks/mpv/default.rb
+++ cookbooks/mpv/default.rb
@@ -1,16 +1,21 @@
+directory '~/.config/mpv' do
+  path "#{ENV['HOME']}/.config/mpv"
+  action :nothing
+end
+
+template '~/.config/mpv/mpv.conf' do
+  source :auto
+  path "#{ENV['HOME']}/.config/mpv/mpv.conf"
+  action :nothing
+end
+
 case node[:platform]
 when 'darwin'
   execute 'brew install mpv' do
     not_if 'which mpv'
+    notifies :create, 'directory[~/.config/mpv]'
+    notifies :create, 'template[~/.config/mpv/mpv.conf]'
   end
 else
   raise NotImplementedError
-end
-
-execute '''cat <<EOF >> ~/.config/mpv/mpv.conf
-# https://www.reddit.com/r/mpv/comments/9gopck/how_would_one_make_all_files_loop_by_default/
-loop=yes
-EOF
-''' do
-  not_if 'grep loop=yes ~/.config/mpv/mpv.conf'
 end
