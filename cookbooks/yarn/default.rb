fcf9e7378221115fff646754fe426e4ef7bd75c6
chck <shimekiri.today@gmail.com>
2020-04-15 00:48:01 +0900

    Fix the command to install yarn from brew to curl

--- cookbooks/yarn/default.rb
+++ cookbooks/yarn/default.rb
@@ -1,6 +1,6 @@
 case node[:platform]
 when 'darwin'
-  execute 'brew install yarn --without-node' do
+  execute 'curl -o- -L https://yarnpkg.com/install.sh | bash' do
     not_if 'which yarn'
   end
 else
