3a730c4cbc785e5589260ad47424d35b90455412
chck <shimekiri.today@gmail.com>
2020-08-01 04:48:10 +0900

    Change the ordering of installation: vim and anyenv

--- roles/darwin/default.rb
+++ roles/darwin/default.rb
@@ -1,8 +1,8 @@
 include_role 'base'
 
 include_cookbook 'git'
-include_cookbook 'vim'
 include_cookbook 'anyenv'
+include_cookbook 'vim'
 include_cookbook 'zsh'
 include_cookbook 'iterm2'
 include_cookbook 'docker'
