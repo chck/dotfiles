35103bb05132da41cbf8894df1955dd91c53a331
chck <shimekiri.today@gmail.com>
2020-12-31 19:46:02 +0900

    Fix the check whether to exist xquartz

--- cookbooks/mplayer/default.rb
+++ cookbooks/mplayer/default.rb
@@ -1,7 +1,7 @@
 case node[:platform]
 when 'darwin'
   execute 'brew install --cask xquartz' do
-    not_if 'brew cask list | grep xquartz'
+    not_if 'which xquartz'
   end
   execute 'brew install mplayer' do
     not_if 'which mplayer'
