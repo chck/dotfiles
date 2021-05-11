2ae5964a3298c5582e74818890a787aa5cb81802
chck <shimekiri.today@gmail.com>
2021-05-12 03:35:15 +0900

    Resolve https://github.com/pyenv/pyenv/issues/1906

--- config/.zsh/lib/languages.zsh
+++ config/.zsh/lib/languages.zsh
@@ -14,6 +14,10 @@ export PATH=$HOME/.poetry/bin:$PATH
 export PIPENV_VENV_IN_PROJECT=true
 # https://github.com/pypa/pipenv/issues/1914
 export PIPENV_SKIP_LOCK=true
+# https://github.com/pyenv/pyenv/issues/1906#issuecomment-834751771
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init --path)"
+fi
 
 # Deno
 export PATH=$HOME/.deno/bin:$PATH
