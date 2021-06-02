f0197a00e9855d3de9276b93f7032da1e3448337
chck <shimekiri.today@gmail.com>
2021-06-02 18:57:41 +0900

    Supress warning for glob no match

--- config/.zsh/lib/basic.zsh
+++ config/.zsh/lib/basic.zsh
@@ -15,6 +15,9 @@ export SAVEHIST=60000000
 # don't logout by EOF
 setopt ignore_eof
 
+# supress warning for glob no match
+setopt nonomatch
+
 # colorized ls
 export LSCOLORS=gxfxcxdxbxegedabagacad
 export LS_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
