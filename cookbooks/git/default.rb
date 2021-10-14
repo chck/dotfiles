a6791c5c2513fed0d82afddcc2ecfe00f8789006
chck <shimekiri.today@gmail.com>
2021-10-14 19:36:58 +0900

    Add dependency to git: git-lfs

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -1,6 +1,17 @@
 package 'git'
 
-dotfile '.gitconfig'
+dotfile '.gitconfig' do
+  not_if 'test -f ~/.gitconfig'
+end
 dotfile '.gitignore_global' do
   not_if 'test -f ~/.gitignore_global'
+end
+
+case node[:platform]
+when 'darwin'
+  execute 'brew install git-lfs' do
+    not_if `git lfs version`.include?("darwin")
+  end
+else
+  raise NotImplementedError
 end
