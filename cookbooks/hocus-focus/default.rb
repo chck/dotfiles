0026e9ba0cd8662276c10fd030aac0868101b4ef
chck <shimekiri.today@gmail.com>
2021-12-26 13:15:11 +0900

    Remove cookbook: hocus-focus

--- cookbooks/hocus-focus/default.rb
+++
@@ -1,8 +0,0 @@
-case node[:platform]
-when 'darwin'
-  execute 'brew install --cask hocus-focus' do
-    not_if 'test -d /Applications/Hocus\ Focus.app/'
-  end
-else
-  raise NotImplementedError
-end
