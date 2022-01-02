e2a028c572ccdf567d2487152f27ef25288a993d
chck <shimekiri.today@gmail.com>
2022-01-02 19:07:40 +0900

    Remove cookbook: git-lfs

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -6,12 +6,3 @@ end
 dotfile '.gitignore_global' do
   not_if 'test -f ~/.gitignore_global'
 end
-
-case node[:platform]
-when 'darwin'
-  execute 'brew install git-lfs' do
-    not_if `git lfs version`.include?("darwin")
-  end
-else
-  raise NotImplementedError
-end
