f1b6c59f7ef631f88bc9599bdf7c3d3077f1d9ab
chck <shimekiri.today@gmail.com>
2022-01-02 22:05:05 +0900

    Modify the installation of google-cloud-sdk

--- cookbooks/gcloud/default.rb
+++ cookbooks/gcloud/default.rb
@@ -1,16 +1,13 @@
-#github_binary 'google-cloud-sdk' do
-#  repository 'google-cloud-sdk/google-cloud-sdk'
-#  version 'v213.0.0'
-#  binary_path 'google-cloud-sdk-213.0.0'
-#end
-
-#execute '/usr/local/bin/google-cloud-sdk/install.sh --quiet'
-
-execute 'sh -c "$(curl -fsSL https://sdk.cloud.google.com)"' do
-  not_if "which gcloud"
+case node[:platform]
+when 'darwin'
+  execute 'brew install --cask google-cloud-sdk' do
+    not_if "which gcloud"
+  end
+else
+  raise NotImplementedError
 end
 
 github_binary "cloud_sql_proxy" do
-  raw_url "https://storage.googleapis.com/cloudsql-proxy/v1.17/cloud_sql_proxy.darwin.amd64"
+  raw_url "https://storage.googleapis.com/cloudsql-proxy/v1.27.1/cloud_sql_proxy.darwin.amd64"
   not_if "which cloud_sql_proxy"
 end
