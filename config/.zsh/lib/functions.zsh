64be1659c593cd222787c11506c32c7a7fd061f6
chck <shimekiri.today@gmail.com>
2022-01-07 02:59:40 +0900

    Add function to clean downloads folder

--- config/.zsh/lib/functions.zsh
+++ config/.zsh/lib/functions.zsh
@@ -1,3 +1,9 @@
 function ip() {
 	echo $(ipconfig getifaddr en0)
 }
+
+function clean_downloads() {
+	# :param duration: Filter downloads based on the file modification time in fd command such as "7 days".
+	duration=$1
+	fd -0 --changed-before "${duration}" --hidden . ~/Downloads | xargs -0 -I {} mv {} ~/.Trash
+}
