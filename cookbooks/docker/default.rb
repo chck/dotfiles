a82ff45005726338ec65e3d90a4345b452969fd4
chck <shimekiri.today@gmail.com>
2022-01-06 23:28:55 +0900

    R.I.P. docker desktop, hello lima

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -1,10 +1,37 @@
 case node[:platform]
 when 'darwin'
-  execute 'brew install --cask docker' do
-    not_if 'which docker'
-  end
   execute 'brew install docker-slim' do
     not_if 'which docker-slim'
+  end
+  case `uname -m`.chomp
+  when 'x86_64'  # Intel Mac
+    execute 'brew install --cask docker' do
+      not_if 'which docker'
+    end
+  when 'arm64'  # M1 Mac
+    execute 'brew install lima' do
+      not_if 'which lima'
+    end
+    package 'qemu' do
+      not_if 'which qemu-system-aarch64'
+    end
+    execute 'brew install docker' do
+      not_if 'which docker'
+    end
+    docker_compose_version = '2.2.2'
+    docker_compose_path = '~/.local/bin/docker-compose'
+    execute "curl -L https://github.com/docker/compose/releases/download/v#{docker_compose_version}/docker-compose-darwin-aarch64 -o #{docker_compose_path} && sudo chmod +x #{docker_compose_path}" do
+      not_if 'which docker-compose'
+    end
+    dotfile 'docker.yaml'
+    execute '''cat <<EOF >> ~/.zsh/lib/apps.zsh
+
+# LIMA
+export DOCKER_HOST=unix://${HOME}/.lima/docker/sock/docker.sock
+EOF
+    ''' do
+      not_if 'grep DOCKER_HOST ~/.zsh/lib/apps.zsh'
+    end
   end
 else
   raise NotImplementedError
@@ -30,7 +57,6 @@ EOF
 ''' do
   not_if 'grep kubectl ~/.zsh/lib/aliases.zsh'
 end
-
 
 execute '''cat <<EOF >> ~/.zsh/lib/aliases.zsh
 # Docker
