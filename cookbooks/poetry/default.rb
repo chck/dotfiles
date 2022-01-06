4c6f0a87109f20e9f6035519cd707a52be0a214c
chck <shimekiri.today@gmail.com>
2022-01-06 18:06:39 +0900

    Replace the declaration of the poetry PATH

--- cookbooks/poetry/default.rb
+++ cookbooks/poetry/default.rb
@@ -7,14 +7,6 @@ else
   raise NotImplementedError
 end
 
-execute '''cat <<EOF >> ~/.zsh/lib/languages.zsh
-# Python
-export PATH=$HOME/.local/bin:$PATH
-EOF
-''' do
-  not_if 'grep Python ~/.zsh/lib/languages.zsh'
-end
-
 execute 'poetry completions zsh > $(brew --prefix)/share/zsh/site-functions/_poetry' do
   not_if 'test -f $(brew --prefix)/share/zsh/site-functions/_poetry'
 end
