0dec5b8935ceffd140a51bf6df89c600a6b740d8
chck <shimekiri.today@gmail.com>
2022-01-22 04:58:05 +0900

    Add the installation of tldr for ubuntu

--- cookbooks/tldr/default.rb
+++ cookbooks/tldr/default.rb
@@ -3,6 +3,10 @@ when 'darwin'
   execute 'brew tap tldr-pages/tldr && brew install tldr' do
     not_if 'which tldr'
   end
+when 'ubuntu'
+  execute 'sudo apt install -y tldr' do
+    not_if 'which tldr'
+  end
 else
   raise NotImplementedError
 end
