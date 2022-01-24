190b91e15a0869a69d8c5130ce210b6a032a46a6
chck <shimekiri.today@gmail.com>
2022-01-24 21:46:31 +0900

    Fix the condition of installation for tableau

--- cookbooks/tableau/default.rb
+++ cookbooks/tableau/default.rb
@@ -1,7 +1,7 @@
 case node[:platform]
 when 'darwin'
   execute 'brew install --cask tableau' do
-    not_if 'test -d /Applications/Tableau.app/'
+    not_if 'test -d /Applications/Tableau*.app'
   end
 else
   raise NotImplementedError
