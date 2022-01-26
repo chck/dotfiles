b916fd2d921d028a564d930178d219eb4be4f856
chck <shimekiri.today@gmail.com>
2022-01-26 22:04:37 +0900

    Fix the installation of gcloud in linux

--- cookbooks/gcloud/default.rb
+++ cookbooks/gcloud/default.rb
@@ -4,7 +4,7 @@ when 'darwin'
     not_if 'which gcloud'
   end
 when 'ubuntu'
-  execute 'sudo apt install -y apt-transport-https ca-certificates gnupg && echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add - && sudo apt update -y && sudo apt install -y google-cloud-sdk' do
+  execute 'curl https://sdk.cloud.google.com | bash' do
     not_if 'which gcloud'
   end
 else
