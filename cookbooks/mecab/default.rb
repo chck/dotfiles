51ab08be7a576675f81478e41df65eef610b3247
chck <shimekiri.today@gmail.com>
2022-06-01 16:55:27 +0900

    Add condition to install mecab for ubuntu

--- cookbooks/mecab/default.rb
+++ cookbooks/mecab/default.rb
@@ -1,2 +1,9 @@
-package 'mecab'
-package 'mecab-ipadic'
+case node[:platform]
+when 'darwin'
+  package 'mecab'
+  package 'mecab-ipadic'
+when 'ubuntu'
+  execute 'sudo apt install -y mecab libmecab-dev mecab-ipadic-utf8' do
+    not_if 'which mecab'
+  end
+end
