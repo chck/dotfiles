272952553c277310673b106f92d7931b12a3ddc8
chck <shimekiri.today@gmail.com>
2022-06-23 19:31:57 +0900

    Add git lfs to git cookbook

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -6,3 +6,17 @@ end
 dotfile '.gitignore_global' do
   not_if 'test -f ~/.gitignore_global'
 end
+
+case node[:platform]
+when 'darwin'
+  execute 'brew install git-lfs' do
+    not_if 'git lfs'
+  end
+when 'ubuntu'
+  execute ' sudo apt install -y git-lfs' do
+    not_if 'git lfs'
+  end
+else
+  raise NotImplementedError
+end
+
