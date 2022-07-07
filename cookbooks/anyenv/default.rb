33ae41b9f0b41dc07b3228cc4f6ef9c84247a5c1
chck <shimekiri.today@gmail.com>
2022-07-08 04:07:26 +0900

    Add installation of rbenv

--- cookbooks/anyenv/default.rb
+++ cookbooks/anyenv/default.rb
@@ -40,8 +40,15 @@ if node[:platform] == 'ubuntu'
     not_if "dpkg -l | grep '^ii' | grep liblzma-dev"
   end
 end
-
 python_version = "3.8.12"
 execute "pyenv install #{python_version} && pyenv global #{python_version} && pip install -U pip && pip install cython pynvim" do
   not_if "pyenv versions | grep #{python_version}"
+end
+
+execute "anyenv install rbenv" do
+  not_if "which rbenv"
+end
+ruby_version = "2.7.6"
+execute "rbenv install #{ruby_version} && rbenv global #{ruby_version}" do
+  not_if "rbenv versions | grep #{ruby_version}"
 end
