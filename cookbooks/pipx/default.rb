aece6c209db68acf5974e944432105bdac25acb8
chck <shimekiri.today@gmail.com>
2022-10-11 19:27:37 +0900

    Add cookbook: pipx

---
+++ cookbooks/pipx/default.rb
@@ -0,0 +1,12 @@
+case node[:platform]
+when 'darwin'
+  execute 'brew install pipx' do
+    not_if 'which pipx'
+  end
+when 'ubuntu'
+  execute 'python3 -m pip install --user pipx' do
+    not_if 'which pipx'
+  end
+else
+  raise NotImplementedError
+end
