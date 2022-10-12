255dfd8b7322ee945d3af040e8449847acee7136
chck <shimekiri.today@gmail.com>
2022-10-13 03:57:10 +0900

    Modify installation of lemonade in arm64

--- cookbooks/lemonade/default.rb
+++ cookbooks/lemonade/default.rb
@@ -12,11 +12,22 @@ when 'x86_64'
   arch = '386'
 when 'amd64'
   arch = 'amd64'
+when 'arm64'
+  arch = 'arm64'
 else
   raise NotImplementedError
 end
 
-github_binary 'lemonade' do
-  raw_url "https://github.com/lemonade-command/lemonade/releases/download/v1.1.1/lemonade_#{platform}_#{arch}.tar.gz"
-  not_if "which lemonade"
+version = "v1.1.2"
+if arch == 'arm64'
+  execute "go install github.com/lemonade-command/lemonade@#{version} && "\
+          "cd $GOPATH/1.19.2/pkg/mod/github.com/lemonade-command/lemonade@#{version} && "\
+          "GOOS=#{platform} GOARCH=#{arch} sudo go build -ldflags '-s -w -X github.com/lemonade-command/lemonade/lemon.Version=#{version}' -o /usr/local/bin/lemonade" do
+    not_if "which lemonade"
+  end
+else
+  github_binary 'lemonade' do
+    raw_url "https://github.com/lemonade-command/lemonade/releases/download/#{version}/lemonade_#{platform}_#{arch}.tar.gz"
+    not_if "which lemonade"
+  end
 end
