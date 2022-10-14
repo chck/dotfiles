b1d8b2321b7d19b66259b7e3f7360cf7d4b390cf
chck <shimekiri.today@gmail.com>
2022-10-15 03:36:24 +0900

    Add installation related to sshfs in darwin

--- cookbooks/lsyncd/default.rb
+++ cookbooks/lsyncd/default.rb
@@ -3,6 +3,12 @@ when 'darwin'
   execute 'brew install rsync lsyncd lua' do
     not_if 'which rsync && which lsyncd && which lua'
   end
+  execute 'brew install --cask macfuse' do
+    not_if 'brew list --cask | grep macfuse'
+  end
+  execute 'brew install gromgit/fuse/sshfs-mac fswatch progress' do
+    not_if 'which sshfs && which fswatch && which progress'
+  end
 else
   raise NotImplementedError
 end
