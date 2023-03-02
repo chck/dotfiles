c8ecc55bacd47caf32f4fd3c83c2bcf54b230eb8
chck <shimekiri.today@gmail.com>
2023-03-02 18:52:31 +0900

    Upgrade python version: 3.8.12 to 3.9.16

--- cookbooks/anyenv/default.rb
+++ cookbooks/anyenv/default.rb
@@ -41,7 +41,7 @@ if node[:platform] == 'ubuntu'
     not_if "dpkg -l | grep '^ii' | grep liblzma-dev"
   end
 end
-python_version = "3.8.12"
+python_version = "3.9.16"
 execute "pyenv install #{python_version} && pyenv global #{python_version} && pip install -U pip && pip install cython pynvim" do
   not_if "pyenv versions | grep #{python_version}"
 end
--- cookbooks/poetry/default.rb
+++ cookbooks/poetry/default.rb
@@ -17,6 +17,9 @@ when 'darwin'
     not_if 'test -f $(brew --prefix)/share/zsh/site-functions/_poetry'
   end
 when 'ubuntu'
+  execute 'mkdir ~/.zfunc && poetry completions zsh > ~/.zfunc/_poetry' do
+    not_if 'test -f ~/.zfunc/_poetry'
+  end
 end
 
 execute 'poetry config virtualenvs.in-project true' do
