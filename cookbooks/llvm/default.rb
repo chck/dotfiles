f3e27c63c84ff1c36dba21ed11b43381997bdb04
chck <shimekiri.today@gmail.com>
2023-04-26 02:56:16 +0900

    Add apt-repository for llvm in linux

--- cookbooks/llvm/default.rb
+++ cookbooks/llvm/default.rb
@@ -4,7 +4,7 @@ when 'darwin'
     not_if 'which llvm@11'
   end
 when 'ubuntu'
-  execute 'sudo apt install -y llvm-9' do
+  execute 'sudo apt-add-repository -y "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-9 main" && sudo apt update -y && sudo apt install -y llvm-9' do
     not_if 'which llvm-config-9'
   end
 else
