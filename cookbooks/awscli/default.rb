babecf40e5430d826539f54038ba4eb80f48c1d7
chck <shimekiri.today@gmail.com>
2023-04-26 02:47:40 +0900

    Add cookbook: awscli for ubuntu

--- cookbooks/awscli/default.rb
+++ cookbooks/awscli/default.rb
@@ -6,6 +6,13 @@ when 'darwin'
   execute 'brew install saml2aws' do
     not_if 'which saml2aws'
   end
+when 'ubuntu'
+  execute 'sudo apt install -y awscli' do
+    not_if 'which aws'
+  end
+  execute 'CURRENT_VERSION=$(curl -Ls https://api.github.com/repos/Versent/saml2aws/releases/latest | grep "tag_name" | cut -d"v" -f2 | cut -d"\"" -f1) && wget -c "https://github.com/Versent/saml2aws/releases/download/v${CURRENT_VERSION}/saml2aws_${CURRENT_VERSION}_linux_amd64.tar.gz" -O - | tar -xzv -C ~/.local/bin' do
+    not_if 'which saml2aws'
+  end
 else
   raise notimplementederror
 end
