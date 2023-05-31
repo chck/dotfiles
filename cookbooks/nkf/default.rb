26e580302bd12203d5c0263ba8eb456695acbde0
chck <shimekiri.today@gmail.com>
2023-05-31 22:42:07 +0900

    Add condition to install nkf

--- cookbooks/nkf/default.rb
+++ cookbooks/nkf/default.rb
@@ -4,9 +4,13 @@ when 'darwin'
     not_if 'which nkf'
   end
 when 'ubuntu'
-  execute 'sudo apt install -y nkf'
+  execute 'sudo apt install -y nkf' do
+    not_if 'which nkf'
+  end
 when 'redhat'
-  execute 'yum install nkf'
+  execute 'yum install nkf' do
+    not_if 'which nkf'
+  end
 else
   raise NotImplementedError
 end
