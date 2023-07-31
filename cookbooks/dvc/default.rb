be947c8e1cff394c1ad51c658a0c9bf54264341d
chck <shimekiri.today@gmail.com>
2023-07-31 18:30:56 +0900

    Add cookbook: dvc for ubuntu

--- cookbooks/dvc/default.rb
+++ cookbooks/dvc/default.rb
@@ -3,6 +3,10 @@ when 'darwin'
   execute 'brew install dvc' do
     not_if 'which dvc'
   end
+when 'ubuntu'
+  execute 'pipx install "dvc[gs]"' do
+    not_if 'which dvc'
+  end
 else
   raise NotImplementedError
 end
