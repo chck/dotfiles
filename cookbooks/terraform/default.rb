0e2c3a368d06436ac9426ba7990c453c6c42e7e4
chck <shimekiri.today@gmail.com>
2023-08-17 20:45:04 +0900

    Add installation: tflint

--- cookbooks/terraform/default.rb
+++ cookbooks/terraform/default.rb
@@ -3,6 +3,9 @@ when 'darwin'
   execute 'brew install terraform' do
     not_if 'which terraform'
   end
+  execute 'brew install tflint' do
+    not_if 'which tflint'
+  end
 when 'ubuntu'
   execute 'curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - && sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main" && sudo apt install -y terraform
 ' do
