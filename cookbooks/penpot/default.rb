5224b404e00e46d0679f1e25be74c1c96c6612f8
chck <shimekiri.today@gmail.com>
2023-08-30 04:58:37 +0900

    Tweak

--- cookbooks/penpot/default.rb
+++ cookbooks/penpot/default.rb
@@ -1,7 +1,7 @@
 case node[:platform]
 when 'darwin'
   execute 'wget https://sudovanilla.com/distribute/applications/penpot-desktop/latest/Penpot.dmg -P /tmp && open /tmp/Penpot.dmg' do
-    not_if 'test -d /Applications/Penpot Desktop.app/'
+    not_if 'test -d /Applications/Penpot\ Desktop.app/'
   end
 else
   raise NotImplementedError
--- cookbooks/vectornator/default.rb
+++ cookbooks/vectornator/default.rb
@@ -1,7 +1,8 @@
 case node[:platform]
 when 'darwin'
   execute 'mas install 1219074514' do
-    not_if 'test -d /Applications/Vectornator.app/'
+    # Vectornator has been renamed to Curve
+    not_if 'test -d /Applications/Curve.app/'
   end
 else
   raise NotImplementedError
