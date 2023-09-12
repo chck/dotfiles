4e4552beb91b1661bdb28f7043f854bea974a1bb
chck <shimekiri.today@gmail.com>
2023-09-12 09:59:28 +0900

    Add tk-dev for pyenv in ubuntu

--- cookbooks/anyenv/default.rb
+++ cookbooks/anyenv/default.rb
@@ -33,15 +33,16 @@ execute "anyenv install -f pyenv" do
   not_if "which pyenv"
 end
 if node[:platform] == 'ubuntu'
-  execute "sudo apt install -y libbz2-dev libreadline-dev libsqlite3-dev lzma liblzma-dev" do
+  execute "sudo apt install -y libbz2-dev libreadline-dev libsqlite3-dev lzma liblzma-dev tk-dev" do
     not_if "dpkg -l | grep '^ii' | grep libbz2-dev"
     not_if "dpkg -l | grep '^ii' | grep libreadline-dev"
     not_if "dpkg -l | grep '^ii' | grep libsqlite3-dev"
     not_if "dpkg -l | grep '^ii' | grep lzma"
     not_if "dpkg -l | grep '^ii' | grep liblzma-dev"
+    not_if "dpkg -l | grep '^ii' | grep tk-dev"
   end
 end
-python_version = "3.9.16"
+python_version = "3.9.18"
 execute "pyenv install #{python_version} && pyenv global #{python_version} && pip install -U pip && pip install cython pynvim" do
   not_if "pyenv versions | grep #{python_version}"
 end
