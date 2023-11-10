aa359ca0ff0d0a8d4f19ed03c81aa81a2950f7f2
chck <shimekiri.today@gmail.com>
2023-11-10 17:25:15 +0900

    Add gh extension: gh-copilot

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -25,5 +25,23 @@ when 'darwin'
   execute 'brew install gh' do
     not_if 'which gh'
   end
+when 'ubuntu'
+  # ref: https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian-ubuntu-linux-raspberry-pi-os-apt
+  cmd = <<EOS
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+EOS
+  execute cmd do
+    not_if 'which gh'
+  end
+else
+  raise NotImplementedError
 end
 
+execute 'gh extension install github/gh-copilot' do 
+  not_if 'gh extension list | grep github/gh-copilot'
+end
