fc33a9792c8d47aed94b49c914709e0cc01b7c68
chck <shimekiri.today@gmail.com>
2024-04-07 23:34:18 +0900

    Add cookbook: displaylink for anker 564

---
+++ cookbooks/displaylink/default.rb
@@ -0,0 +1,8 @@
+case node[:platform]
+when 'darwin'
+  execute 'brew install --cask displaylink' do
+    not_if 'test -d /Applications/DisplayLink Manager.app/'
+  end
+else
+  raise NotImplementedError
+end
