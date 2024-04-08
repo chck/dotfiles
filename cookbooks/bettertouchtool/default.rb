12391c933b516b04231182e3702b0ffaca4a43e8
chck <shimekiri.today@gmail.com>
2024-04-08 23:41:18 +0900

    Remove cookbook: bettertouchtool

--- cookbooks/bettertouchtool/default.rb
+++
@@ -1,8 +0,0 @@
-case node[:platform]
-when 'darwin'
-  execute 'brew install --cask bettertouchtool' do
-    not_if 'test -d /Applications/BetterTouchTool.app/'
-  end
-else
-  raise NotImplementedError
-end
