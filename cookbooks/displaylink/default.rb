c0aae383ab13472230e555df1d249484e785cc06
chck <shimekiri.today@gmail.com>
2024-04-10 21:57:07 +0900

    Fix condition for installations

--- cookbooks/displaylink/default.rb
+++ cookbooks/displaylink/default.rb
@@ -1,7 +1,7 @@
 case node[:platform]
 when 'darwin'
   execute 'brew install --cask displaylink' do
-    not_if 'test -d /Applications/DisplayLink Manager.app/'
+    not_if 'test -d /Applications/DisplayLink\ Manager.app'
   end
 else
   raise NotImplementedError
--- cookbooks/elgato-stream-deck/default.rb
+++ cookbooks/elgato-stream-deck/default.rb
@@ -1,7 +1,7 @@
 case node[:platform]
 when 'darwin'
   execute 'brew install --cask elgato-stream-deck' do
-    not_if 'test -d /Applications/Elgato Stream Deck.app/'
+    not_if 'test -d /Applications/Elgato\ Stream\ Deck.app'
   end
 else
   raise NotImplementedError
