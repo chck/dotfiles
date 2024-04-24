7f6ff0fff7eb12fe0d21b209b472e7f682ebb173
chck <shimekiri.today@gmail.com>
2024-04-24 19:39:23 +0900

    Apply cache and lazy load for zsh

--- config/.zsh/lib/functions.zsh
+++ config/.zsh/lib/functions.zsh
@@ -1,3 +1,16 @@
+function source() {
+  ensure_zcompiled $1
+  builtin source $1
+}
+
+function ensure_zcompiled() {
+  local compiled="$1.zwc"
+  if [[ ! -r "$compiled" || "$1" -nt "$compiled" ]]; then
+    echo "\033[1;36mCompiling\033[m $1"
+    zcompile $1
+  fi
+}
+
 function ip() {
   echo $(ipconfig getifaddr en0)
 }
--- config/.zsh/lib/plugins.zsh
+++ config/.zsh/lib/plugins.zsh
@@ -2,5 +2,18 @@
 export ENHANCD_HOOK_AFTER_CD=ls
 export ENHANCD_FILTER="fzf --height 40%:fzy"
 
-eval "$(sheldon source)"
+# sheldon - zsh plugin manager
+# zcompile source files
+ensure_zcompiled ~/.zshrc
+# cache tips: https://zenn.dev/fuzmare/articles/zsh-plugin-manager-cache
+cache_dir=${XDG_CACHE_HOME:-$HOME/.cache}
+sheldon_cache="$cache_dir/sheldon.zsh"
+sheldon_toml="$HOME/.config/sheldon/plugins.toml"
+if [[ ! -r "$sheldon_cache" || "$sheldon_toml" -nt "$sheldon_cache" ]]; then
+  sheldon source > $sheldon_cache
+fi
+source "$sheldon_cache"
+unset cache_dir sheldon_cache sheldon_toml
+
+# starship - shell framework
 eval "$(starship init zsh)"
