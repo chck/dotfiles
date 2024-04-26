7baa25d2f4ba71de67532d123bde7f2f77cf9f52
chck <shimekiri.today@gmail.com>
2024-04-26 18:53:18 +0900

    Update geekbench path

--- cookbooks/geekbench/default.rb
+++ cookbooks/geekbench/default.rb
@@ -1,7 +1,7 @@
 case node[:platform]
 when 'darwin'
   execute 'brew install --cask geekbench' do
-    not_if 'test -d /Applications/Geekbench\ 5.app'
+    not_if 'test -d /Applications/Geekbench\ 6.app'
   end
 else
   raise NotImplementedError
