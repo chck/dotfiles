b6a784c7c41d94249bd2d942bd7c11f577300b61
chck <shimekiri.today@gmail.com>
2024-05-14 18:51:11 +0900

    Fix app path: curve to linearity curve

--- cookbooks/vectornator/default.rb
+++ cookbooks/vectornator/default.rb
@@ -1,8 +1,8 @@
 case node[:platform]
 when 'darwin'
   execute 'mas install 1219074514' do
-    # Vectornator has been renamed to Curve
-    not_if 'test -d /Applications/Curve.app/'
+    # Vectornator has been renamed to Linearity Curve
+    not_if 'test -d /Applications/Linearity\ Curve.app/'
   end
 else
   raise NotImplementedError
