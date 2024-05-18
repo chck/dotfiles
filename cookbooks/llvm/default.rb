a3e99d7eabb8bba30034f5a1a9740624c55a53e6
chck <shimekiri.today@gmail.com>
2024-05-18 23:49:24 +0900

    Organize cookbook: llvm

--- cookbooks/llvm/default.rb
+++ cookbooks/llvm/default.rb
@@ -1,7 +1,7 @@
 case node[:platform]
 when 'darwin'
-  execute 'brew install llvm@11' do
-    not_if 'brew info llvm@11 -q'
+  execute 'brew install llvm' do
+    not_if 'brew info llvm -q'
   end
 when 'ubuntu'
   execute 'sudo apt-add-repository -y "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-9 main" && sudo apt update -y && sudo apt install -y llvm-9' do
