8e42c3f19474b3ec475c620755bc2d46388cf1f4
chck <shimekiri.today@gmail.com>
2024-05-18 23:58:36 +0900

    Replace deprecated cookbook: youtube-dl to yt-dlp

--- cookbooks/yt-dlp/default.rb
+++ cookbooks/yt-dlp/default.rb
@@ -1,7 +1,7 @@
 case node[:platform]
 when 'darwin'
-  execute 'brew install youtube-dl' do
-    not_if 'which youtube-dl'
+  execute 'brew install yt-dlp' do
+    not_if 'which yt-dlp'
   end
 else
   raise NotImplementedError
