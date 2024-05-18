b672e43a80526cbb458ccf22a26e53f98513f029
chck <shimekiri.today@gmail.com>
2024-05-18 23:40:19 +0900

    Remove cookbook: grammarly

--- cookbooks/grammarly/default.rb
+++
@@ -1,8 +0,0 @@
-case node[:platform]
-when 'darwin'
-  execute 'brew install --cask grammarly' do
-    not_if 'test -d /Applications/Grammarly\ Editor.app'
-  end
-else
-  raise NotImplementedError
-end
