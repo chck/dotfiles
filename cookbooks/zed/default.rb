9ecfbb0403f138e1c8a97b7a8dc2b043b8591ca4
chck <shimekiri.today@gmail.com>
2024-09-07 17:18:43 +0900

    Add settings for zed

--- cookbooks/zed/default.rb
+++ cookbooks/zed/default.rb
@@ -3,6 +3,9 @@ when 'darwin'
   execute 'brew install --cask zed' do
     not_if 'test -d /Applications/Zed.app'
   end
+  dotfile "zed/settings.json" do
+    destination "#{ENV['HOME']}/.config"
+  end
 when 'ubuntu'
   execute 'curl -f https://zed.dev/install.sh | sh' do
     not_if 'which zed'
