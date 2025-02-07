a3cd5514335ccc4fbee26b0fb9795a0d7b0c43d0
chck <shimekiri.today@gmail.com>
2025-02-08 08:48:54 +0900

    Remove elgato-wave-link to manage manually

--- cookbooks/elgato-wave-link/default.rb
+++
@@ -1,8 +0,0 @@
-case node[:platform]
-when 'darwin'
-  execute 'brew install --cask elgato-wave-link' do
-    not_if 'test -d /Applications/Elgato\ Wave\ Link.app'
-  end
-else
-  raise NotImplementedError
-end
