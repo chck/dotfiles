e3518009e20fb10c25b077ab3ecd023591cfe7a8
chck <shimekiri.today@gmail.com>
2025-04-26 18:50:01 +0900

    feat: bump up ctop version: 0.7.1 to 0.7.7

--- cookbooks/ctop/default.rb
+++ cookbooks/ctop/default.rb
@@ -1,10 +1,13 @@
+version = "0.7.7"
+url_prefix = "https://github.com/bcicen/ctop/releases/download/v#{version}/ctop-#{version}"
+
 case node[:platform]
 when 'darwin'
-  url = 'https://github.com/bcicen/ctop/releases/download/v0.7.1/ctop-0.7.1-darwin-amd64'
+  url = "#{url_prefix}-darwin-amd64"
 when 'ubuntu'
-  url = 'https://github.com/bcicen/ctop/releases/download/v0.7.1/ctop-0.7.1-linux-amd64'
+  url = "#{url_prefix}-linux-amd64"
 when 'redhat'
-  url = 'https://github.com/bcicen/ctop/releases/download/v0.7.1/ctop-0.7.1-linux-arm64'
+  url = "#{url_prefix}-linux-arm64"
 else
   raise NotImplementedError
 end
