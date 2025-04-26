ce41b16a967737caa879f68a30b3c2775e860b14
chck <shimekiri.today@gmail.com>
2025-04-26 17:31:28 +0900

    refactor: split installation into darwin and ubuntu

--- cookbooks/mise/default.rb
+++ cookbooks/mise/default.rb
@@ -1,5 +1,21 @@
-execute 'curl https://mise.run | sh' do
-  not_if 'which mise'
+case node[:platform]
+when 'darwin'
+  execute 'brew install mise' do
+    not_if 'which mise'
+  end
+when 'ubuntu'
+  execute '''
+sudo apt update -y && sudo apt install -y gpg sudo wget curl
+sudo install -dm 755 /etc/apt/keyrings
+wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1> /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+sudo apt update
+sudo apt install -y mise
+''' do
+    not_if 'which mise'
+  end
+else
+  raise NotImplementedError
 end
 
 # python
