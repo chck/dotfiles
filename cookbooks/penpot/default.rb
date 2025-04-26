1043dd08dcb3b5d8620df0e1e976df01ded616fa
chck <shimekiri.today@gmail.com>
2025-04-26 20:04:37 +0900

    fix: replace penpot host url

--- cookbooks/penpot/default.rb
+++ cookbooks/penpot/default.rb
@@ -1,6 +1,6 @@
 case node[:platform]
 when 'darwin'
-  execute 'wget https://sudovanilla.com/distribute/applications/penpot-desktop/latest/Penpot.dmg -P /tmp && open /tmp/Penpot.dmg' do
+  execute 'wget https://github.com/author-more/penpot-desktop/releases/download/v0.13.0/Penpot-Desktop-0.13.0-arm64.dmg -P /tmp && open /tmp/Penpot-Desktop-0.13.0-arm64.dmg' do
     not_if 'test -d /Applications/Penpot\ Desktop.app/'
   end
 else
