4eee3dbd5c0efee3abb70e4ba765c196bee83638
chck <shimekiri.today@gmail.com>
2025-05-06 20:33:23 +0900

    refactor: organize mise configs

--- config/.zsh/lib/languages.zsh
+++ config/.zsh/lib/languages.zsh
@@ -1,4 +1,4 @@
-# mise (asdf)
+# Mise
 eval "$(~/.local/bin/mise activate zsh)"
 
 # Rust
@@ -14,7 +14,13 @@ export PATH=$HOME/.deno/bin:$PATH
 
 # JavaScript
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$HOME/.local/bin:$PATH"
+export PNPM_HOME="~/.local/share/mise/installs/pnpm/latest/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
 
+# Python
 export PIPENV_VENV_IN_PROJECT=true
 # https://github.com/pypa/pipenv/issues/1914
 export PIPENV_SKIP_LOCK=true
