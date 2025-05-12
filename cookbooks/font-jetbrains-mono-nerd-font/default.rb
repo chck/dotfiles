f22001a6dc595cd809375890bb38a521d8f9e670
chck <shimekiri.today@gmail.com>
2025-05-12 20:07:28 +0900

    fix: replace command to check font exists

--- cookbooks/font-jetbrains-mono-nerd-font/default.rb
+++ cookbooks/font-jetbrains-mono-nerd-font/default.rb
@@ -1,7 +1,7 @@
 case node[:platform]
 when 'darwin'
   execute 'brew install --cask font-jetbrains-mono-nerd-font' do
-    not_if 'test ~/Library/Fonts/JetBrainsMonoNerdFont-Regular.ttf'
+    not_if 'test -f ~/Library/Fonts/JetBrainsMonoNerdFont-Regular.ttf'
   end
 else
   raise NotImplementedError
